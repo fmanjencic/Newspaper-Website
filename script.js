10a3b9e8d313d363600620c60beb31036ae1812f
Filip Manjencic <76404759+fmanjencic@users.noreply.github.com>
2023-07-19 23:50:09 -0400

    uploading changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const closeMenu = () => {
     let x = document.getElementById('mobile-menu')
+    let y = document.getElementById('mobile-navbar');
     x.style.display = "none";
+    y.style.display = "block";
 }
 
 const openMenu = () => {
-    let x = document.getElementById('mobile-menu')
-    let y = document.getElementById('mobile-navbar')
-    x.style.display = "block";
-    x.style.visibility = "visible";
+    let x = document.getElementById('mobile-menu');
+    let y = document.getElementById('mobile-navbar');
     y.style.display = "none";
+    x.style.display = "block";
 }
+
+//On Load
+//closeMenu();
